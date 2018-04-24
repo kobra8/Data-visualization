4d82d2a2f961b11fa4cb5351046e9d60ddaedb79
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2018-04-24 15:40:50 +0200

    Column rect like chart

--- 2.08/js/main.js
+++ 2.08/js/main.js
@@ -4,11 +4,22 @@
 *    2.8 - Activity: Your first visualization!
 */
 
-var svg = d3.select("#chart-area").append("svg");
 
 d3.json("data/buildings.json").then((data)=> {
     data.map(data => data.height = +data.height);
     console.log(data);
+    
+    var svg = d3.select("#chart-area").append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+    var columns = svg.selectAll("rect").data(data);
+
+    columns.enter().append("rect")
+    .attr("x", (d, i)=> i * 50)
+    .attr("y", 50)
+    .attr("width", 30)
+    .attr("height", (d)=> d.height)
+    .attr("fill", "blue");
 
 })
 .catch(error => console.log(error));
