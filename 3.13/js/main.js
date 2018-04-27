f75c732575c32c93748517442e71e5631ca18696
kobra8 <jerzydrobot@gmail.com>
2018-04-27 22:41:02 +0200

    Project1 varibles, canvas, group, and axis bottom

--- 3.13/js/main.js
+++ 3.13/js/main.js
@@ -1,19 +1,37 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    Project 1 - Star Break Coffee
-*/
-var svg = d3.select("#chart-area").append("svg");
+/* Project 1 - Star Break Coffee */
 
-svg.attr("width", 600).attr("height", 400);
+var margin = {left: 40, right: 20, top: 10, bottom: 30};
+var width = 600 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
-d3.json("data/revenues.json").then( res => {
-  console.log(res);
+var g = d3.select("#chart-area")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("transform", "translate("+ margin.left + "," + margin.top +")");
 
+d3.json("data/revenues.json").then( data => {
+  data.map( data => data.revenue = + data.revenue)
+  console.log(data);
 
+  var x = d3.scaleBand()
+      .domain(data.map( d => d.month))
+      .range([0, width])
+      .paddingInner(0.3)
+      .paddingOuter(0.3);
 
+  var y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.revenue)])
+      .range([height, 0]);  
 
-  svg.append("rect")
+  var xAxisCall = d3.axisBottom(x);
+    g.append("g")
+    .attr("class", "x-axis")
+    .call(xAxisCall)
+    .attr("transform","translate(0, "+ height +")")
+    .attr("y", "10")
+
+  g.append("rect")
     .attr("x",20)
     .attr("y",20)
     .attr("width",20)
